89254b1d48936f6f80fab4d6baf1a9d6bb770c0e
mostafijur014 <mostafijur014@gmail.com>
2024-12-13 20:49:40 +0600

    PHP Practice day 1

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo"Hello Horld";
+//Variable 
+//Data Type
+//Data Type Check
+
+//$line_gap = ".\n";
+$userName = 'Mostafijur' ;
+$number = 12345;
+$flote = 1.44;
+$boolean = true;
+$array = ['Mango', 'Orange', 'Apple'];
+$blanck = null;
+
+echo $userName;
+echo $number;
+echo $flote;
+echo $array[2];
+echo $blanck;
+
+var_dump($userName);
+var_dump($number);
+var_dump($flote);
+var_dump($boolean);
+var_dump($array);
+var_dump($blanck);
+
+
 ?>
