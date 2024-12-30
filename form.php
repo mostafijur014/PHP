0bc3e521c18d5ae951064abd4f51d514d92433c0
mostafijur014 <mostafijur014@gmail.com>
2024-12-30 17:17:07 +0600

    Form Handling

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="form_data.php" method="post" enctype="multipart/form-data">
+    <input type="text" placeholder="Name" name="username"></br></br>
+    <input type="email" placeholder="Email" name="email"> </br> </br>
+    <input type="file" placeholder="File" name="file"> </br> </br>
+    <input type="submit" placeholder="Send">
+    </form>
+</body>
+</html>
---
+++ form_data.php
@@ -0,0 +1,14 @@
+<?php
+// echo"<pre>";
+// print_r($_REQUEST);
+echo "<pre>";
+$submitedData = $_REQUEST;
+
+print_r($_REQUEST);
+echo "<pre>";
+echo $submitedData['email'];
+echo "<pre>";
+print_r($_POST);
+print_r($_FILES);
+
+?>
